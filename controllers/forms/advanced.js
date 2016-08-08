585180ba9767a7c65e28946500d9ec3e90327191
Rodrigo Azevedo <rodrigo4zevedo@gmail.com>
2016-08-08 18:53:45 -0300

    Advanced Form page must be fixed

--- controllers/forms/advanced.js
+++ controllers/forms/advanced.js
@@ -37,29 +37,29 @@ app.controller('advancedController', function($scope, $ocLazyLoad) {
     autoclose: true
   });
 
-  //iCheck for checkbox and radio inputs
-  $('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
-    checkboxClass: 'icheckbox_minimal-blue',
-    radioClass: 'iradio_minimal-blue'
-  });
-  //Red color scheme for iCheck
-  $('input[type="checkbox"].minimal-red, input[type="radio"].minimal-red').iCheck({
-    checkboxClass: 'icheckbox_minimal-red',
-    radioClass: 'iradio_minimal-red'
-  });
-  //Flat red color scheme for iCheck
-  $('input[type="checkbox"].flat-red, input[type="radio"].flat-red').iCheck({
-    checkboxClass: 'icheckbox_flat-green',
-    radioClass: 'iradio_flat-green'
-  });
+  // //iCheck for checkbox and radio inputs
+  // $('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
+  //   checkboxClass: 'icheckbox_minimal-blue',
+  //   radioClass: 'iradio_minimal-blue'
+  // });
+  // //Red color scheme for iCheck
+  // $('input[type="checkbox"].minimal-red, input[type="radio"].minimal-red').iCheck({
+  //   checkboxClass: 'icheckbox_minimal-red',
+  //   radioClass: 'iradio_minimal-red'
+  // });
+  // //Flat red color scheme for iCheck
+  // $('input[type="checkbox"].flat-red, input[type="radio"].flat-red').iCheck({
+  //   checkboxClass: 'icheckbox_flat-green',
+  //   radioClass: 'iradio_flat-green'
+  // });
 
   //Colorpicker
-  $(".my-colorpicker1").colorpicker();
+  // $(".my-colorpicker1").colorpicker();
   //color picker with addon
-  $(".my-colorpicker2").colorpicker();
+  // $(".my-colorpicker2").colorpicker();
 
   //Timepicker
-  $(".timepicker").timepicker({
-    showInputs: false
-  });
+  // $(".timepicker").timepicker({
+  //   showInputs: false
+  // });
 });
